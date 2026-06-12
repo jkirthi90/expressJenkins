8bf2889a236dff6285c7ce26e4b6f9aae5412f8e
jkirthi90 <jkirthi90@gmail.com>
2026-06-12 12:38:40 +0530

    Update index.js

--- examples/auth/index.js
+++ examples/auth/index.js
@@ -130,5 +130,5 @@ app.post('/login', function (req, res, next) {
 /* istanbul ignore next */
 if (!module.parent) {
   app.listen(3000);
-  console.log('Express started on port 3000');
+  console.log('Express started on port 3000 successfully');
 }
